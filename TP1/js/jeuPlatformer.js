b5273b31654d55283b059b0e977715fc0628c287
Raphaël Jeudy <45304194+LosTacosDeZelda@users.noreply.github.com>
2020-04-24 14:11:42 -0400

    Add files via upload

---
+++ TP1/js/jeuPlatformer.js
@@ -0,0 +1,12 @@
+
+window.addEventListener("load", function(){
+
+    let config = {
+        scale:{
+            width:300,
+            height:400
+        }
+    }
+
+    window.game = new Phaser.Game(config);
+})
